1e6406fd37594e4e47fbfbcbbd776d5a1899d6c4
Andre Leite <andreleite@MacBook-Air-de-Andre.local>
2022-02-03 21:56:24 -0300

    only god knows

---
+++ app/src/main/java/com/example/appbars/commons/App.kt
@@ -0,0 +1,28 @@
+package com.example.appbars.commons
+
+import android.app.Application
+import com.example.appbars.map.di.MapsModules
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidFileProperties
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            // use AndroidLogger as Koin Logger - default Level.INFO
+            androidLogger()
+
+            // use the Android context given there
+            androidContext(this@App)
+
+            // load properties from assets/koin.properties file
+            androidFileProperties()
+            modules(
+                MapsModules().mapViewModels()
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/example/appbars/map/di/MapsModules.kt
@@ -0,0 +1,9 @@
+package com.example.appbars.map.di
+
+import org.koin.dsl.module
+
+internal class MapsModules {
+    fun mapViewModels() = module {
+
+    }
+}
